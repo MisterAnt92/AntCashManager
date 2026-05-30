20d989175a37942a90c45ce5b0066fa866cd7b52
Simone Formica <simonfor12@gmail.com>
2026-05-30 17:47:24 +0200

    Uograde versionCode and versionName

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sformica.ant_cashmanager"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.5.0"
+        versionCode = 6
+        versionName = "1.5.1"
     }
 
     buildTypes {
