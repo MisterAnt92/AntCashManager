b2a6c3167acc167986aeca354e69a1ac6fc53346
Simone Formica <simonfor12@gmail.com>
2026-05-30 09:22:28 +0200

    chore(build): update version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sformica.ant_cashmanager"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.4.7"
+        versionCode = 5
+        versionName = "1.5.0"
     }
 
     buildTypes {
