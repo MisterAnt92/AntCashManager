508b86c6a89e8e439d051985757a17c2a9aab680
Simone Formica <simonfor12@gmail.com>
2026-06-12 08:27:59 +0200

    chore(release): update versionCode to 7 and versionName to 1.5.2 feat(manifest): add camera feature and remove AD_ID permission

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sformica.ant_cashmanager"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.5.1"
+        versionCode = 7
+        versionName = "1.5.2"
     }
 
     buildTypes {
